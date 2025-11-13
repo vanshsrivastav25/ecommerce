6075e7156aa74eb6ffc84fe9bf6abc78f12afe20
Vansh Srivastav <vanshsrivastavjnp25@gmail.com>
2025-11-13 22:22:41 +0530

    Initial commit

---
+++ src/components/common/FeaturedProducts.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import eleven from "../../assets/images/eleven.jpg";
+
+const FeaturedProducts = () => {
+  return (
+    <div>
+      <section className="section-2 py-5">
+        <div className="container">
+          <h2>Featured Products</h2>
+          <div className="row mt-4">
+            <div className="col-md-3">
+              <div className="product-card border-0">
+                <div className="card-img">
+                  <img src={eleven} alt="" className="w-10" />
+                </div>
+
+                <div className="card-body pt-3">
+                  <a href="">Red Check Shirt for Mens</a>
+                  <div className="price">
+                    $50{" "}
+                    <span className="text-decoration-linr-through">$80</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-3">
+              <div className="product-card border-0">
+                <div className="card-img">
+                  <img src={eleven} alt="" className="w-10" />
+                </div>
+
+                <div className="card-body pt-3">
+                  <a href="">Red Check Shirt for Mens</a>
+                  <div className="price">
+                    $50{" "}
+                    <span className="text-decoration-linr-through">$80</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-3">
+              <div className="product-card border-0">
+                <div className="card-img">
+                  <img src={eleven} alt="" className="w-10" />
+                </div>
+
+                <div className="card-body pt-3">
+                  <a href="">Red Check Shirt for Mens</a>
+                  <div className="price">
+                    $50{" "}
+                    <span className="text-decoration-linr-through">$80</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-3">
+              <div className="product-card border-0">
+                <div className="card-img">
+                  <img src={eleven} alt="" className="w-10" />
+                </div>
+
+                <div className="card-body pt-3">
+                  <a href="">Red Check Shirt for Mens</a>
+                  <div className="price">
+                    $50{" "}
+                    <span className="text-decoration-linr-through">$80</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
---
+++ src/components/shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const shop = () => {
+  return (
+    <div>
+      Shop Page
+    </div>
+  )
+}
+
+export default shop
