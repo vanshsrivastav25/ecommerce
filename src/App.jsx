cc7fffffc5f84deb786684b7358f5d13aa2bd909
Vansh Srivastav <vanshsrivastavjnp25@gmail.com>
2025-11-15 07:52:35 +0530

    Create Cart Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './components/home'
-import Shop from './components/shop'
-import Product from './components/Product'
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/home";
+import Shop from "./components/shop";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <>
-     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/shop' element={<Shop/>}/>
-        <Route path='/product' element={<Product/>}/>
-      </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
